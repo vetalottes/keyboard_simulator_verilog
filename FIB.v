b5be0c0a981c1a329f584374b44a8c83ae8e0023
veta <124678022+vetalottes@users.noreply.github.com>
2023-02-08 08:19:53 +0300

    Update FIB.v

--- FIB.v
+++ FIB.v
@@ -2,12 +2,12 @@
 
 module FIB(input clk, input [7:0] code, input set_signal, input reset_signal, output reg res_out, output reg [7:0] res_code); // output reg lamp);
 
-// code - код нажатой клавиши
-// set_signal\reset_signal - сигналы установки\сброса
-// res_out - true/false в зависимости от сравнения - сигнал
-// res_code - код эталонной клавиши, который передается в вга для отображения на мониторе
-reg [2:0] state; // состояния автомата
-reg [7:0] need_symbol; // эталонное значение, с которым сравнивается нажатая клавиша
+// code - РєРѕРґ РЅР°Р¶Р°С‚РѕР№ РєР»Р°РІРёС€Рё
+// set_signal\reset_signal - СЃРёРіРЅР°Р»С‹ СѓСЃС‚Р°РЅРѕРІРєРё\СЃР±СЂРѕСЃР°
+// res_out - true/false true/false РІ Р·Р°РІРёСЃРёРјРѕСЃС‚Рё РѕС‚ СЃСЂР°РІРЅРµРЅРёСЏ (1 С‚РѕР»СЊРєРѕ РІ СЃР»СѓС‡Р°Рµ, РєРѕРіРґР° СЃРёРјРІРѕР»С‹ РЅРµ СЂР°РІРЅС‹)
+// res_code - РєРѕРґ СЌС‚Р°Р»РѕРЅРЅРѕР№ РєР»Р°РІРёС€Рё, РєРѕС‚РѕСЂС‹Р№ РїРµСЂРµРґР°РµС‚СЃСЏ РІ vga РґР»СЏ РѕС‚РѕР±СЂР°Р¶РµРЅРёСЏ
+reg [2:0] state;
+reg [7:0] need_symbol; // СЌС‚Р°Р»РѕРЅРЅРѕРµ Р·РЅР°С‡РµРЅРёРµ, СЃ РєРѕС‚РѕСЂС‹Рј СЃСЂР°РІРЅРёРІР°РµС‚СЃСЏ РЅР°Р¶Р°С‚Р°СЏ РєР»Р°РІРёС€Р°
 
 initial
 begin
@@ -23,98 +23,95 @@ begin
         state <= 3'd1;
     end
     case (state)
-		3'd0: // задаем первое "нереальное" эталонное, которое равно симолу включения клавиатурного тренажера
+		3'd0: // Р·Р°РґР°РµРј РїРµСЂРІРѕРµ "РЅРµСЂРµР°Р»СЊРЅРѕРµ" СЌС‚Р°Р»РѕРЅРЅРѕРµ, РєРѕС‚РѕСЂРѕРµ СЂР°РІРЅРѕ СЃРёРјРІРѕР»Сѓ РІРєР»СЋС‡РµРЅРёСЏ РєР»Р°РІРёР°С‚СѓСЂРЅРѕРіРѕ С‚СЂРµРЅР°Р¶РµСЂР°
 		begin
-			need_symbol[7:0] <= 8'h5A; // тот символ, который запускает прогу - enter
+			need_symbol[7:0] <= 8'h5A; // С‚РѕС‚ СЃРёРјРІРѕР», РєРѕС‚РѕСЂС‹Р№ Р·Р°РїСѓСЃРєР°РµС‚ РІСЃРµ - enter
 			state <= 3'd2;
 		end
-		3'd1: // начало
+		3'd1: // РЅР°С‡Р°Р»Рѕ
 		begin
 		    res_out <= 0;
-            if ((code[7:0] == 8'h5A) || (code[7:0] == 8'h00)) // если запускаем прогу (клавиша enter) или сброс (тогда заново) или неизвестная клавиша
+			if ((code[7:0] == 8'h5A) || (code[7:0] == 8'h00)) // РµСЃР»Рё Р·Р°РїСѓСЃРєР°РµРј (РєР»Р°РІРёС€Р° enter) РёР»Рё РЅРµРёР·РІРµСЃС‚РЅР°СЏ РєР»Р°РІРёС€Р°
                 state <= 3'd0;
             else 
                 state <= 3'd2;
 		end
-		3'd2: //сравнение
+		3'd2: // СЃСЂР°РІРЅРµРЅРёРµ
 		begin
 		  if ((code[7:0] == need_symbol[7:0]) && (need_symbol[7:0] == 8'h5A)) begin
-		      res_code[7:0] <= 8'h1B; // первое нормальное(!) эталонное (S), которое и нужно будет нажимать на клавиатуре
-		      //res_out <= 1; 
-		      state <= 3'd5;
+			res_code[7:0] <= 8'h1B; // РїРµСЂРІРѕРµ РЅРѕСЂРјР°Р»СЊРЅРѕРµ(!) СЌС‚Р°Р»РѕРЅРЅРѕРµ (S), РєРѕС‚РѕСЂРѕРµ Рё РЅСѓР¶РЅРѕ Р±СѓРґРµС‚ РЅР°Р¶РёРјР°С‚СЊ РЅР° РєР»Р°РІРёР°С‚СѓСЂРµ
+		      state <= 3'd4;
 		  end
 		  if ((code[7:0] == need_symbol[7:0]) && (need_symbol[7:0] != 8'h5A)) begin
-		      //res_out <= 1; 
-		      state <=3'd3; // задать новое эталонное
+		      state <=3'd3; // Р·Р°РґР°С‚СЊ РЅРѕРІРѕРµ СЌС‚Р°Р»РѕРЅРЅРѕРµ
 		  end
 		  if (code != need_symbol) begin
 		      res_out <= 1; 
-		      state <=3'd4; // эталонное остается прежним
+		      state <=3'd4; // РѕСЃС‚Р°РІРёС‚СЊ СЌС‚Р°Р»РѕРЅРЅРѕРµ РїСЂРµР¶РЅРёРј
 		  end
 		end
-		3'd3: //задаем эталонное
+		3'd3: // Р·Р°РґР°РµРј СЌС‚Р°Р»РѕРЅРЅРѕРµ
 		begin
-            case (need_symbol)
-                8'h1B: need_symbol <= 8'h1C; // S->A
-                8'h1C: need_symbol <= 8'h34; // A->G
-                8'h34: need_symbol <= 8'h4D; // P
-                8'h4D: need_symbol <= 8'h45; // 0
-                8'h45: need_symbol <= 8'h1D; // W
-                8'h1D: need_symbol <= 8'h25; // 4
-                8'h25: need_symbol <= 8'h4E; // -
-                8'h4E: need_symbol <= 8'h22; // X
-                8'h22: need_symbol <= 8'h15; // Q
-                8'h15: need_symbol <= 8'h16; // 1
-                8'h16: need_symbol <= 8'h2A; // V
-                8'h2A: need_symbol <= 8'h3D; // 7
-                8'h3D: need_symbol <= 8'h43; // I
-                8'h43: need_symbol <= 8'h41; // ,
-                8'h41: need_symbol <= 8'h23; // D
-                8'h23: need_symbol <= 8'h2E; // 5
-                8'h2E: need_symbol <= 8'h3E; // 8
-                8'h3E: need_symbol <= 8'h3A; // M
-                8'h3A: need_symbol <= 8'h2D; // R
-                8'h2D: need_symbol <= 8'h42; // K
-                8'h42: need_symbol <= 8'h5B; // ]
-                8'h5B: need_symbol <= 8'h32; // B
-                8'h32: need_symbol <= 8'h2C; // T
-                8'h2C: need_symbol <= 8'h49; // .
-                8'h49: need_symbol <= 8'h21; // C
-                8'h21: need_symbol <= 8'h54; // [
-                8'h54: need_symbol <= 8'h3C; // U
-                8'h3C: need_symbol <= 8'h1A; // Z
-                8'h1A: need_symbol <= 8'h4C; // ;
-                8'h4C: need_symbol <= 8'h46; // 9
-                8'h46: need_symbol <= 8'h1E; // 2
-                8'h1E: need_symbol <= 8'h52; // '
-                8'h52: need_symbol <= 8'h2B; // F
-                8'h2B: need_symbol <= 8'h3B; // J
-                8'h3B: need_symbol <= 8'h55; // =
-                8'h55: need_symbol <= 8'h24; // E
-                8'h24: need_symbol <= 8'h4B; // L
-                8'h4B: need_symbol <= 8'h31; // N
-                8'h31: need_symbol <= 8'h5D; // \
-                8'h5D: need_symbol <= 8'h4A; // /
-                8'h4A: need_symbol <= 8'h33; // H
-                8'h33: need_symbol <= 8'h44; // O
-                8'h44: need_symbol <= 8'h26; // 3
-                8'h26: need_symbol <= 8'h35; // Y
-                8'h35: need_symbol <= 8'h36; // 6
-                8'h36: need_symbol <= 8'h1B; // S (заново цикл)
-            endcase
-            state <=3'd4;
+            		case (need_symbol)
+			8'h1B: need_symbol <= 8'h1C; // S->A
+			8'h1C: need_symbol <= 8'h34; // A->G
+			8'h34: need_symbol <= 8'h4D; // P
+			8'h4D: need_symbol <= 8'h45; // 0
+			8'h45: need_symbol <= 8'h1D; // W
+			8'h1D: need_symbol <= 8'h25; // 4
+			8'h25: need_symbol <= 8'h4E; // -
+			8'h4E: need_symbol <= 8'h22; // X
+			8'h22: need_symbol <= 8'h15; // Q
+			8'h15: need_symbol <= 8'h16; // 1
+			8'h16: need_symbol <= 8'h2A; // V
+			8'h2A: need_symbol <= 8'h3D; // 7
+			8'h3D: need_symbol <= 8'h43; // I
+			8'h43: need_symbol <= 8'h41; // ,
+			8'h41: need_symbol <= 8'h23; // D
+			8'h23: need_symbol <= 8'h2E; // 5
+			8'h2E: need_symbol <= 8'h3E; // 8
+			8'h3E: need_symbol <= 8'h3A; // M
+			8'h3A: need_symbol <= 8'h2D; // R
+			8'h2D: need_symbol <= 8'h42; // K
+			8'h42: need_symbol <= 8'h5B; // ]
+			8'h5B: need_symbol <= 8'h32; // B
+			8'h32: need_symbol <= 8'h2C; // T
+			8'h2C: need_symbol <= 8'h49; // .
+			8'h49: need_symbol <= 8'h21; // C
+			8'h21: need_symbol <= 8'h54; // [
+			8'h54: need_symbol <= 8'h3C; // U
+			8'h3C: need_symbol <= 8'h1A; // Z
+			8'h1A: need_symbol <= 8'h4C; // ;
+			8'h4C: need_symbol <= 8'h46; // 9
+			8'h46: need_symbol <= 8'h1E; // 2
+			8'h1E: need_symbol <= 8'h52; // '
+			8'h52: need_symbol <= 8'h2B; // F
+			8'h2B: need_symbol <= 8'h3B; // J
+			8'h3B: need_symbol <= 8'h55; // =
+			8'h55: need_symbol <= 8'h24; // E
+			8'h24: need_symbol <= 8'h4B; // L
+			8'h4B: need_symbol <= 8'h31; // N
+			8'h31: need_symbol <= 8'h5D; // \
+			8'h5D: need_symbol <= 8'h4A; // /
+			8'h4A: need_symbol <= 8'h33; // H
+			8'h33: need_symbol <= 8'h44; // O
+			8'h44: need_symbol <= 8'h26; // 3
+			8'h26: need_symbol <= 8'h35; // Y
+			8'h35: need_symbol <= 8'h36; // 6
+			    8'h36: need_symbol <= 8'h1B; // S (Р·Р°РЅРѕРІРѕ С†РёРєР»)
+            	endcase
+            	state <=3'd4;
 		end
-		3'd4: begin // результат для вга
-		  res_code[7:0] <= need_symbol[7:0]; 
-		  //res_out <= 0;
-		  if (set_signal) begin
-		      state <= 3'd1; // если пришло новое значение символа с клавиатуры
-		  end
-		end
-		3'd5: begin // результат для вга после включения клавиатурного тренажера
-		      need_symbol <= 8'h1B;
-		      //res_out <= 0;
-		      if (set_signal) state <= 3'd1;
+		3'd4: begin // СЂРµР·СѓР»СЊС‚Р°С‚ РґР»СЏ vga
+			 if (code[7:0] == 8'h5A) begin
+			 	need_symbol <= 8'h1B;
+			 end
+			 else begin
+			 	res_code[7:0] <= need_symbol[7:0];
+			 end
+			 if (set_signal) begin
+				 state <= 3'd1; // РµСЃР»Рё РїСЂРёС€Р»Рѕ РЅРѕРІРѕРµ Р·РЅР°С‡РµРЅРёРµ СЃРёРјРІРѕР»Р° СЃ РєР»Р°РІРёР°С‚СѓСЂС‹
+			 end
 		end
 		default: state = 3'd1;
 	endcase
